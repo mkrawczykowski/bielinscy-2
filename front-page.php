9430a994e4c81cfdd5f995162d14cbda0cf15386
Michał Krawczykowski <mkrawczykowski@gmail.com>
2021-02-25 17:52:39 +0100

    Navbar with dropdown

--- front-page.php
+++ front-page.php
@@ -3,13 +3,7 @@
 get_template_part( 'template-parts/header', 'main' );
 ?>
 
-<div class="container">
-  <div class="row">
-    <div class="col">s</div>
-    <div class="col">d</div>
-    <div class="col">f</div>
-  </div>
-</div>
+
 
 <?php
 get_template_part( 'template-parts/footer', 'main');
