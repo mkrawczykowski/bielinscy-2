b10b0b898c1dd454de8a54378c01bb04f35039e6
Michał Krawczykowski <mkrawczykowski@gmail.com>
2021-02-27 13:38:28 +0100

    Changed width of columns in header

--- template-parts/header-main.php
+++ template-parts/header-main.php
@@ -13,10 +13,10 @@
   <div class="header__main-nav">
     <div class="container py-4">
       <div class="row">
-        <div class="col-6 justify-content-center">
+        <div class="col-6 col-md-4 justify-content-center">
           <a href="" class="header__logo"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-bielinscy-90.webp" width="80" height="80" alt="Zakład Brązowniczy S.C. Bielińscy - logo"></a>
         </div>
-        <div class="col-6 d-flex justify-content-end align-items-center">
+        <div class="col-6 col-md-8 d-flex justify-content-end align-items-center">
           <input class="header__checkbox" type="checkbox" id="header__checkbox" />
           <label class="header__hamburger-button" for="header__checkbox"><span class="header__icon"></span></label>
           <nav>
