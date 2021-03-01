a24e1f2869e02dd7d2f1364b3e8cee0fcbd4a1ca
Michał Krawczykowski <mkrawczykowski@gmail.com>
2021-03-01 17:27:21 +0100

    Yoast breadcrumbs on frontpage

--- template-parts/frontpage-slider.php
+++ template-parts/frontpage-slider.php
@@ -4,6 +4,11 @@
         <div class="col my-11">
           <h2 class="slider__small-heading">Coś</h2>
           <h1 class="slider__big-heading">Większy tytuł</h1>
+          <?php
+            if ( function_exists('yoast_breadcrumb') ) {
+              yoast_breadcrumb( '<p class="slider__breadcrumbs">','</p>' );
+            }
+          ?>
         </div>
       </div>      
     </div>
