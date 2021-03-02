8c847d0f25e70f0550f55fda294c15c019adc5a3
Michał Krawczykowski <mkrawczykowski@gmail.com>
2021-03-02 22:37:37 +0100

    The content on page

--- page.php
+++ page.php
@@ -2,7 +2,15 @@
 <?php
 get_template_part( 'template-parts/header', 'main' );
 get_template_part( 'template-parts/page', 'title' );
+?>
 
-the_content();
+<div class="container">
+  <div class="row my-5">
+    <div class="col">
+      <?php the_content(); ?>
+    </div>
+  </div>
+</div>
 
+<?php
 get_template_part( 'template-parts/footer', 'main');
