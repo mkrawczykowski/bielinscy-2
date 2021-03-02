1f057c6be9401e3c9c0e43817103744e71c8e9a2
Michał Krawczykowski <mkrawczykowski@gmail.com>
2021-03-02 17:18:23 +0100

    Better slider section with paragraph and button styles

--- template-parts/frontpage-slider.php
+++ template-parts/frontpage-slider.php
@@ -1,14 +1,19 @@
 <div class="slider">
   <div class="container">
-    <div class="row">
-      <div class="col my-11">
+    <div class="row py-11">
+      <div class="col-12 col-lg-6">
         <h2 class="slider__small-heading">Coś</h2>
         <h1 class="slider__big-heading">Większy tytuł</h1>
-        <?php
+        <p class="slider__paragraph">At Farite church, you can spread the love to everyone, reach lost & broken people and meet them right where they are. Our church is a place where people share love and care for one another.</p>
+        <a class="button button-color-2">Dowiedz się więcej</a>
+        <!-- <?php
           if ( function_exists('yoast_breadcrumb') ) {
             yoast_breadcrumb( '<p class="slider__breadcrumbs">','</p>' );
           }
-        ?>
+        ?> -->
+      </div>
+      <div class="col-12 col-lg-6">
+        s
       </div>
     </div>      
   </div>
