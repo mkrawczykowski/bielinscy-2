bedb83d978d7b9a9b4c94f000b9d724877456f03
Michał Krawczykowski <mkrawczykowski@gmail.com>
2021-03-13 18:46:06 +0100

    Main menu without hamburger and columns in mega dropdown

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,19 +1,25 @@
 window.addEventListener('DOMContentLoaded', (event) => {
   const itemsWithSubmenus = document.querySelectorAll('.menu-item-has-children');
   // const items = document.querySelectorAll('.logo-nav__main-menu .menu-item a');
-  console.log(itemsWithSubmenus);
+  // console.log(itemsWithSubmenus);
 
   for (const itemID in itemsWithSubmenus) {
-    console.log(itemID);
+    // console.log(itemID);
     if (itemsWithSubmenus.hasOwnProperty(itemID)) {
       const item = itemsWithSubmenus[itemID];
-      console.log(item);
+      // console.log(item);
       item.addEventListener('click', (e) => {
-        e.preventDefault();
+        console.log(e.target);
+        console.log('klik');
+        (e.target.closest('ul.sub-menu')) ? console.log('tak') : e.preventDefault();
+        // console.log((e.path).indexOf('ul.sub-menu'));
+        // console.log(e);
+        // console.log(item);
+        ;
         if (item.classList.contains('menu-item-has-children')) {
 
           item.classList.toggle('dropdown-active');
-          console.log('dropdown-active');
+          // console.log('dropdown-active');
         }
       });
 
