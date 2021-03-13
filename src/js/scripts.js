c55678f38edad5ce94b4a0504c9468b3a8313369
Michał Krawczykowski <mkrawczykowski@gmail.com>
2021-03-13 21:31:47 +0100

    Minor changes to many things

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,29 +1,24 @@
 window.addEventListener('DOMContentLoaded', (event) => {
   const itemsWithSubmenus = document.querySelectorAll('.menu-item-has-children');
-  // const items = document.querySelectorAll('.logo-nav__main-menu .menu-item a');
-  // console.log(itemsWithSubmenus);
 
   for (const itemID in itemsWithSubmenus) {
-    // console.log(itemID);
     if (itemsWithSubmenus.hasOwnProperty(itemID)) {
       const item = itemsWithSubmenus[itemID];
-      // console.log(item);
       item.addEventListener('click', (e) => {
-        console.log(e.target);
-        console.log('klik');
         (e.target.closest('ul.sub-menu')) ? console.log('tak') : e.preventDefault();
-        // console.log((e.path).indexOf('ul.sub-menu'));
-        // console.log(e);
-        // console.log(item);
-        ;
         if (item.classList.contains('menu-item-has-children')) {
-
           item.classList.toggle('dropdown-active');
-          // console.log('dropdown-active');
         }
       });
-
     }
   };
+
+  const hamburgerToggler = document.querySelector('.logo-nav__hamburger-menu');
+  const mainMenu = document.querySelector('.logo-nav__main-menu');
+
+  hamburgerToggler.addEventListener('click', (e) => {
+    mainMenu.classList.toggle('active');
+    hamburgerToggler.classList.toggle('open');
+  });
 
 });
